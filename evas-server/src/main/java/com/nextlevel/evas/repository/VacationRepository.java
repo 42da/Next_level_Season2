71f53f287ee91ddfd9d85ccca2d49668ec307bad
leeseoeun <seoeun.shuen@gmail.com>
2024-02-07 21:08:11 +0900

    update vacation and vacationhistory `crud`

--- evas-server/src/main/java/com/nextlevel/evas/repository/VacationRepository.java
+++ evas-server/src/main/java/com/nextlevel/evas/repository/VacationRepository.java
@@ -3,7 +3,7 @@ package com.nextlevel.evas.repository;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import com.nextlevel.evas.domain.Vacation;
-import com.nextlevel.evas.domain.VacationHistory;
+import com.nextlevel.evas.domain.VacationDetailDate;
 
 @Mapper
 public interface VacationRepository {
@@ -12,12 +12,14 @@ public interface VacationRepository {
   int update(Vacation vacation);
   int delete(int idx);
 
-  int insertHistory(List<VacationHistory> vacationList);
+  int insertHistory(List<VacationDetailDate> vacationList);
   int deleteHistory(int vacationIdx);
 
   Vacation findByIdx(int idx);
   List<Vacation> findAllApplicationByEmployeeId(String employeeId);
   List<Vacation> findAllVacationByEmployeeId(String employeeId);
   List<Vacation> findAllCalendar();
+
+  String findByVacationIdx(int vacationidx);
 
 }
